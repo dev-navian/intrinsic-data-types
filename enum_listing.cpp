df53588f0db164e6cee3e021060b7b50347f60c5
dev-navian <ivan.annan@turntabl.io>
2021-09-27 08:16:51 +0000

    enum listing exercise

---
+++ enum_listing.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	 enum {RED, YELLOW, AMBER=YELLOW, GREEN};
+	        
+	 cout << "RED is: " << RED << endl;
+	 cout << "YELLOW is: " << YELLOW << endl;
+	 cout << "AMBER is: " << AMBER << endl;
+	 cout << "GREEN is: " << GREEN << endl;
+			        
+}
