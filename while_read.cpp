634e3708f92b6e0ae53e41bf515540c9ae43e398
dev-navian <ivan.annan@turntabl.io>
2021-09-28 08:12:27 +0000

    reading from standard input with cin.get()

---
+++ while_read.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Enter a given text and have it printed back to you: ";
+   
+   char c;
+   while (cin.get(c)) {
+       cout.put(c);
+   }
+    
+}
+
